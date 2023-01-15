d7493ea96d285f045729088429c8fa5aabbcf2d3
Todd Hepler <2554+thepler@users.noreply.github.com>
2023-01-15 16:35:17 -0600

    additional test case

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -14,3 +14,4 @@ import { expect } from "./expect.js"
 expect(containsDuplicate([1,2,3,1]), true);
 expect(containsDuplicate([1,2,3,4]), false);
 expect(containsDuplicate([1,1,1,3,3,4,3,2,4,2]), true);
+expect(containsDuplicate([]), false);
--- maxProfit.js
+++ maxProfit.js
@@ -14,4 +14,5 @@ function maxProfit(prices) {
 import { expect } from "./expect.js"
 
 expect(maxProfit([7,1,5,3,6,4]), 5);
-expect(maxProfit([7,6,4,3,1]), 0)
+expect(maxProfit([7,6,4,3,1]), 0);
+expect(maxProfit([]), 0);
