c3b0d9656951c76ca64df188f385f096872dab32
Todd Hepler <2554+thepler@users.noreply.github.com>
2023-01-15 16:19:53 -0600

    maxProfit implementation
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ maxProfit.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+function maxProfit(prices) {
+    if (prices.length == 0) { return 0 }
+    var maxProfit = 0;
+    var min = prices[0];
+    for (const p of prices) {
+        if (p < min) { min = p }
+        if (p - min > maxProfit) { maxProfit = p - min }
+    }
+    return maxProfit;
+};
+
+import { expect } from "./expect.js"
+
+expect(maxProfit([7,1,5,3,6,4]), 5);
+expect(maxProfit([7,6,4,3,1]), 0)
