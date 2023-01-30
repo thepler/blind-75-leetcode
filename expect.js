e733824cb001259ace5499e6e24df88d122b5977
Todd Hepler <2554+thepler@users.noreply.github.com>
2023-01-30 16:47:43 -0600

    add test number

--- expect.js
+++ expect.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
+var n = 1;
 export function expect(got, expected) {
     // console.log(JSON.stringify(got))
     // console.log(JSON.stringify(expected))
     if (JSON.stringify(got) === JSON.stringify(expected)) {
-        console.log("ok");
+        console.log("ok " + n);
+        n++;
         return;
     }
-    console.error("not ok");
+    console.error("not ok " + n);
     console.error("  got:      " + got);
     console.error("  expected: " + expected);
+    n++;
     return;
 }
