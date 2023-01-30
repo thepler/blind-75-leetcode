f6471c89dede3874396aee8221d45d1e9536b8ed
Todd Hepler <2554+thepler@users.noreply.github.com>
2023-01-30 15:36:32 -0600

    recursive version

--- findMinimumRotatedSortedArray.js
+++ findMinimumRotatedSortedArray.js
@@ -2,30 +2,29 @@
 
 function findMin(nums) {
     if (nums.length === 0) { return }
-    if (nums.length < 2) { return nums[0] }
-    var start = 0;
-    var end = nums.length - 1;
-
-    // nums is in sorted order
-    if (nums[start] < nums[end]) {
-        return nums[start];
-    }
-    while (start < end) {
-        var mid = Math.floor((start+end) / 2);
-        // console.log("mid: " + mid + " " + nums[start] + " " + nums[mid] + " " + nums[end]);
-        if (mid > 0 && nums[mid] < nums[mid-1]) {
-            return nums[mid]
+    if (nums.length === 1) { return nums[0] }
+    const finder = (start, end) => {
+        // console.log(start + " " + end);
+        if (start === end || start + 1 == end) {
+            if (nums[start] < nums[end]) {
+                // console.log("returning start");
+                return nums[start];
+            }
+            else {
+                // console.log("returning end " + end + " " + nums[end]);
+                return nums[end];
+            }
         }
-        if (mid < (nums.length - 1) && nums[mid + 1] < nums[mid]) {
-            return nums[mid + 1]
-        }
-        if (nums[mid] > nums[start]) {
-            start = mid + 1;
+        const mid = Math.floor((start + end) / 2);
+        // console.log(start + " " + end + " " + mid);
+        if (nums[mid] > nums[end]) {
+            return finder(mid, end);
         }
         else {
-            end = mid - 1;
+            return finder(start, mid);
         }
     }
+    return finder(0, nums.length - 1);
 }
 
 import { expect } from "./expect.js"
