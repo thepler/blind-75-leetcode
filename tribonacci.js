5fc82d16ee9e55653311df7cdd4d5c7f27951b01
Todd Hepler <2554+thepler@users.noreply.github.com>
2023-01-30 18:12:11 -0600

    memoize better, up to 37

--- tribonacci.js
+++ tribonacci.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
+// https://leetcode.com/problems/n-th-tribonacci-number/
+
+const memo = {}
 const tribonacci = function(n) {
+    return tribMemo(n, memo);
+}
+const tribMemo = function(n, memo) {
     if (n === 0) { return 0 }
     if (n === 1 || n === 2) { return 1 }
-    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+    if (memo[n]) { return memo[n] }
+    return tribonacci(n - 3, memo) + tribonacci(n - 2, memo) + tribonacci(n - 1, memo)
 }
 
 import { expect } from "./expect.js"
@@ -17,4 +24,6 @@ expect(tribonacci(5), 7);
 expect(tribonacci(6), 13);
 expect(tribonacci(7), 24);
 expect(tribonacci(25), 1389537);
+expect(tribonacci(34), 334745777);
 expect(tribonacci(35), 615693474);
+expect(tribonacci(37), 2082876103);
